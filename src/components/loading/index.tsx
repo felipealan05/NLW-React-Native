0f6600043f308b0e63f18c6ec503376c46e9c3c3
felipealan05 <felipealanh@gmail.com>
2024-12-16 14:20:09 -0300

    Aula 01 - NLW Pocket: Mobile - React Native

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,9 @@
+import { ActivityIndicator } from "react-native";
+
+import { style } from "./styles";
+import { colors } from '@/styles/theme'
+
+export function Loading () 
+{
+    return <ActivityIndicator color = {colors.green.base} style = {style.container} />
+}
